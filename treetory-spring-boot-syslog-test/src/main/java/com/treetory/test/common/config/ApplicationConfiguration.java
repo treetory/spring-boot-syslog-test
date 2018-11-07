9e0d81dba63b56afaadad9312abf76931d662897
treetory <treetory@gmail.com>
2018-11-07 15:07:37 +0900

    Add Swagger

--- treetory-spring-boot-syslog-test/src/main/java/com/treetory/test/common/config/ApplicationConfiguration.java
+++ treetory-spring-boot-syslog-test/src/main/java/com/treetory/test/common/config/ApplicationConfiguration.java
@@ -20,12 +20,13 @@ import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.resource.PathResourceResolver;
 
 import com.treetory.test.mvc.service.SyslogServiceImpl;
 
-@ComponentScan(basePackages = {"com.treetory.test.mvc", "com.treetory.test.common.util"}, 
+@ComponentScan(basePackages = {"com.treetory.test.mvc", "com.treetory.test.common"},
 useDefaultFilters = false,
 includeFilters = {
             @ComponentScan.Filter(value = Controller.class),
@@ -37,7 +38,7 @@ includeFilters = {
 @Configuration
 @EnableAsync
 @EnableWebMvc
-@Import(value={})
+@Import(value={ SwaggerConfiguration.class })
 public class ApplicationConfiguration implements InitializingBean, ApplicationListener<ApplicationEvent>, WebMvcConfigurer {
 
 	@Autowired
@@ -48,7 +49,7 @@ public class ApplicationConfiguration implements InitializingBean, ApplicationLi
      */
     @Bean
     @Description("Prevent the broken euckr character set.")
-    public Filter charactertEncodingFilter() {
+    public Filter characterEncodingFilter() {
     	CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
     	characterEncodingFilter.setEncoding("UTF-8");
     	characterEncodingFilter.setForceEncoding(true);
@@ -58,8 +59,9 @@ public class ApplicationConfiguration implements InitializingBean, ApplicationLi
     @Override
     @Description("Every resources for requesting from view is registerd in here.")
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
+
     	registry.addResourceHandler("/resources/**", "/css/**", "/images/**", "/js/**", "/lib/**", "/fonts/**")
-    	.addResourceLocations(
+    	        .addResourceLocations(
     			"classpath:/resources/",
     			"classpath:/static/css/",
     			"classpath:/static/images/",
@@ -67,9 +69,29 @@ public class ApplicationConfiguration implements InitializingBean, ApplicationLi
     			"classpath:/static/lib/",
     			"classpath:/static/fonts/"
     			)
-    	.setCachePeriod(600).resourceChain(true).addResolver(new PathResourceResolver());
+    	        .setCachePeriod(600).resourceChain(true).addResolver(new PathResourceResolver());
+        /**
+         * swagger-ui 의 웹 리소스를 찾을 수 있도록 리소스 핸들러에 리소스 경로와 위치를 등록한다.
+         */
+    	registry.addResourceHandler("/api/swagger-ui.html**")
+                .addResourceLocations("classpath:/META-INF/resources/swagger-ui.html");
+
+        registry.addResourceHandler("/api/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+
     }
-    
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        /**
+         * swagger-ui 의 url path 의 redirect 주소를 등록한다.
+         */
+        registry.addRedirectViewController("/api/v2/api-docs", "/v2/api-docs");
+        registry.addRedirectViewController("/api/swagger-resources/configuration/ui", "/swagger-resources/configuration/ui");
+        registry.addRedirectViewController("/api/swagger-resources/configuration/security", "/swagger-resources/configuration/security");
+        registry.addRedirectViewController("/api/swagger-resources", "/swagger-resources");
+    }
+
     @Override
     public void afterPropertiesSet() throws Exception {
     	
@@ -89,7 +111,6 @@ public class ApplicationConfiguration implements InitializingBean, ApplicationLi
     		break;
     	case "ContextClosedEvent" :
     		
-    		//appContext.getBean(SyslogServiceImpl.class).destorySyslogServer();
     		appContext.getBean(SyslogServiceImpl.class).destroyWholeSyslogServer();
     		
     		break;
